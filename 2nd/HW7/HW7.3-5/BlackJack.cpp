aea5fd71cb5e76f59325953a2daadf9ed95a6b7d
Alexandr Dodonov <dodon19980404@yandex.ru>
2021-06-09 15:24:36 +0300

    HW7

---
+++ 2nd/HW7/HW7.3-5/BlackJack.cpp
@@ -0,0 +1,49 @@
+﻿
+/*1 Black Jack*/
+#include <iostream> 
+#include <vector>
+#include <set> 
+#include <string> 
+#include "Game.h" 
+
+using namespace std;
+int main()
+{
+    cout << "\t\tWelcome to Blackjack!\n\n";
+
+    int numPlayers = 0;
+    while (numPlayers < 1 || numPlayers > 7)
+    {
+        cout << "How many players? (1 - 7): ";
+        cin >> numPlayers;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << "Incorrect number" << endl;
+        }
+    }
+
+    vector<string> names;
+    string name;
+    for (int i = 0; i < numPlayers; ++i)
+    {
+        cout << "Enter player name: ";
+        cin >> name;
+        names.push_back(name);
+    }
+    cout << endl;
+    cout << "Enter num of decks" << endl;
+    int* num = new int;
+    cin >> *num;
+    Game aGame(names, *num);
+    delete num;
+    char again = 'y';
+    while (again != 'n' && again != 'N')
+    {
+        aGame.play();
+        cout << "\nDo you want to play again? (Y/N): ";
+        cin >> again;
+    }
+    return 0;
+}
