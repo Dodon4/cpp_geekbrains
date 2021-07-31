520d5be478a5056cf9c0c2ee773f79be8c9bcb52
Alexandr Dodonov <dodon19980404@yandex.ru>
2021-07-31 22:53:45 +0300

    full hw

--- 3rd/HW7/HW7.cpp
+++ 3rd/HW7/HW7.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include "student.pb.h"
 using namespace std;
-namespace third {
+//namespace third {
     class IRepository {
         virtual void Open() = 0; // бинарная десериализация в файл
         virtual void Save() = 0; // бинарная сериализация в файл
@@ -12,31 +12,73 @@ namespace third {
         virtual string GetAllInfo(const FullName& name) = 0;
         virtual string GetAllInfo() = 0;
     };
-    class StudentsGroup : public IRepository, public IMethods
+    class StudentGroup : public IRepository, public IMethods
     {
-        vector<Student> sg;
-        StudentsGroup()
+        StudentsGroup _group;
+    public:
+        StudentGroup(StudentsGroup group) { _group.CopyFrom(group); }
+        StudentGroup() {};
         void Open() override
         {
-
+            ifstream in("Group.bin", ios_base::binary);
+            _group.ParseFromIstream(&in);
+            cout << _group.mutable_group(0)->DebugString();
+        }
+        void Save() override
+        {
+            ofstream out("Group.bin", ios_base::binary);
+            _group.SerializeToOstream(&out);
+        }
+        string GetAllInfo() override
+        {
+              return  _group.DebugString();
+        }
+        string GetAllInfo(const FullName& name) override
+        {
+            for (size_t i = 0; i < _group.group_size(); i++)
+                if (_group.mutable_group(i)->mutable_fio()->name() == name.name() &&
+                    _group.mutable_group(i)->mutable_fio()->surname() == name.surname())
+                {
+                     return  _group.mutable_group(i)->DebugString();
+                }
+            cout << "The student with the given name does not exist" << endl;
+            return "-1";
         }
         double GetAverageScore(const FullName& name) override
         {
-            for(auto grade: name.)
+            for(size_t i = 0; i < _group.group_size(); i++)
+                if (_group.mutable_group(i)->mutable_fio()->name() == name.name() &&
+                    _group.mutable_group(i)->mutable_fio()->surname() == name.surname())
+                {
+                    return _group.mutable_group(i)->avg();
+                }
+            cout << "The student with the given name does not exist" << endl;
+            return -1;
         }
     };
-}
+//}
 int main() {
     
-    FullName *fioa = new FullName;
-    fioa->set_name("asd");
-    fioa->set_surname("bcd");
-    Student *Alex = new Student;
-    Alex->set_allocated_fio(fioa);
-    Alex->add_grade(5);
-    Alex->add_grade(3);
-    Alex->add_grade(4);
-    Alex->set_avg(4);
-    StudentsGroup *group = new StudentsGroup;
-    group->add_group();
+    //FullName fioa;
+    //fioa.set_name("asd");
+    //fioa.set_surname("bcd");
+    //FullName aasd;
+    //aasd.set_name("asd");
+    //aasd.set_surname("a");
+    //Student Alex;
+    //Alex.mutable_fio()->CopyFrom(fioa);
+    //Alex.add_grade(5);
+    //Alex.add_grade(3);
+    //Alex.add_grade(4);
+    //Alex.set_avg(4);
+    //StudentsGroup group;
+    //group.add_group();
+    //group.mutable_group(0)->CopyFrom(Alex);
+    //group.add_group();
+    //group.mutable_group(1)->CopyFrom(Alex);
+    StudentGroup a;
+    a.Open();
+    //cout << a.GetAverageScore(aasd) << endl;
+    cout << a.GetAllInfo();
+
 }
